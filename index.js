ae3bf72c224ed856afeb8860f2d48900d3ec8778
Ksena <oksishukh@yahoo.com>
2017-12-22 11:34:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,32 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ // write your code her
+ var str = {[item]: Math.floor( Math.random(1, 101) * 100 ) }
+  cart.push(str);
+ }
+
 }
 
 function viewCart() {
-  // write your code here
+  if (!cart.length) {
+    return console.log("Your shopping cart is empty.")
+  }
+const itemsWithPrices = [];
+var itemNames = Object.keys(cart[i]);
+    var itemName = itemNames[0];
+    var itemPrice = cart[i][itemName]
+
+    itemsWithPrices.push(`${itemName} at $${itemPrice}`)
+  }
+
+  if (itemsWithPrices.length === 1) {
+    return console.log(`In your cart, you have ${itemsWithPrices.join()}.`)
+  } else if (itemsWithPrices.length === 2) {
+    return console.log(`In your cart, you have ${itemsWithPrices[0]} and ${itemsWithPrices.slice(-1)}.`)
+  } else {
+    return console.log(`In your cart, you have ${itemsWithPrices.slice(0,-1).join(', ')}, and ${itemsWithPrices.slice(-1)}.`)
+  }
 }
 
 function total() {
